9b4c458db01ccddf2a5b5f18e25448b1b8e71ce8
Nguyen Huu Nam <nam.d02th@gmail.com>
2021-05-24 16:23:39 +0700

    - illustrate about stateless

--- ReactJs/my-app/src/index.js
+++ ReactJs/my-app/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Stateless from './Stateless';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Stateless/>
   </React.StrictMode>,
   document.getElementById('root')
 );

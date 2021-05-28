ff35c4abc2899b0f8e2514532f0d79e7023f82b3
Nguyen Huu Nam <nam.d02th@gmail.com>
2021-05-28 07:26:38 +0700

    [practice] gift.js: reducer of gif

---
+++ ReactJs/my-app/src/gifsReducer.js
@@ -0,0 +1,37 @@
+
+const INIT_STATE = {
+    data:
+    [
+        {
+          id: 1,
+          url: "https://media0.giphy.com/media/LRsMaCckjrvJdQLn7w/giphy.gif?cid=4e4f4969pm8j3tcs91cmgmujseh757hwsle7p948idjenup0&rid=giphy.gif&ct=g"
+        },
+        {
+          id: 2,
+          url: "https://media0.giphy.com/media/LRsMaCckjrvJdQLn7w/giphy.gif?cid=4e4f4969pm8j3tcs91cmgmujseh757hwsle7p948idjenup0&rid=giphy.gif&ct=g"
+        },
+        {
+          id: 3,
+          url: "https://media0.giphy.com/media/LRsMaCckjrvJdQLn7w/giphy.gif?cid=4e4f4969pm8j3tcs91cmgmujseh757hwsle7p948idjenup0&rid=giphy.gif&ct=g"
+        },
+        {
+          id: 4,
+          url: "https://media0.giphy.com/media/LRsMaCckjrvJdQLn7w/giphy.gif?cid=4e4f4969pm8j3tcs91cmgmujseh757hwsle7p948idjenup0&rid=giphy.gif&ct=g"
+        },
+        {
+          id: 5,
+          url: "https://media0.giphy.com/media/LRsMaCckjrvJdQLn7w/giphy.gif?cid=4e4f4969pm8j3tcs91cmgmujseh757hwsle7p948idjenup0&rid=giphy.gif&ct=g"
+        },
+        {
+          id: 6,
+          url: "https://media0.giphy.com/media/LRsMaCckjrvJdQLn7w/giphy.gif?cid=4e4f4969pm8j3tcs91cmgmujseh757hwsle7p948idjenup0&rid=giphy.gif&ct=g"
+        }
+      ]
+  };
+  
+
+const postsReducers = (state = INIT_STATE) =>{
+    return state;
+}
+
+export default postsReducers;

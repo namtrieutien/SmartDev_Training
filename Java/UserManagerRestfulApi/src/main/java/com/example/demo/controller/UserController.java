73e4f173432158ae869c9ab35d003a657d96a3a6
Nguyen Huu Nam <nam.d02th@gmail.com>
2021-06-04 14:02:09 +0700

    - get list users

--- Java/UserManagerRestfulApi/src/main/java/com/example/demo/controller/UserController.java
+++ Java/UserManagerRestfulApi/src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -13,7 +16,8 @@ public class UserController {
 
     @GetMapping("")
     public ResponseEntity<?> getListUsers(){
-        return null;
+        List<User> users = userService.getListUsers();
+        return ResponseEntity.ok(users);
     }
 
     @PostMapping("")
--- Java/UserManagerRestfulApi/src/main/java/com/example/demo/service/UserServiceImp.java
+++ Java/UserManagerRestfulApi/src/main/java/com/example/demo/service/UserServiceImp.java
@@ -1,8 +1,21 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.User;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class UserServiceImp implements UserService{
-    
+    private static ArrayList<User> users = new ArrayList<>();
+    static {
+        users.add(new User(1,"namtt007","nam@gmail.com", "0938001142", "avatar_namtt007", "password_namtt007"));
+        users.add(new User(2,"weedsquiet","weedsquiet@gmail.com", "0935078541", "avatar_weedsquiet", "password_weedsquiet"));
+    }
+
+    @Override
+    public List<User> getListUsers() {
+        return users;
+    }
 }

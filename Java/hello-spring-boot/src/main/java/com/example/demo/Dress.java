5588297eecea60b85f8dc00bbd7f984306a22187
Nguyen Huu Nam <nam.d02th@gmail.com>
2021-06-04 08:23:14 +0700

    - using annotation @Scope("prototype")

--- Java/hello-spring-boot/src/main/java/com/example/demo/Dress.java
+++ Java/hello-spring-boot/src/main/java/com/example/demo/Dress.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
+
 public class Dress implements Outfit {
     @Override
     public void wear(){
--- Java/hello-spring-boot/src/main/java/com/example/demo/HelloSpringBootApplication.java
+++ Java/hello-spring-boot/src/main/java/com/example/demo/HelloSpringBootApplication.java
@@ -6,21 +6,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Scope;
 
 @ComponentScan("com.example.demo.other_packet")
 @ComponentScan("com.example.demo")
+
 @SpringBootApplication
 public class HelloSpringBootApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(HelloSpringBootApplication.class, args);
 
-		GirlFriend girlFriend = applicationContext.getBean(GirlFriend.class);
-		System.out.println("Instance girlFriend: " + girlFriend);
-		System.out.println("Instance girlFriend.dress: " + girlFriend.dress);
-		girlFriend.dress.wear();
+		Dress dress1 = applicationContext.getBean(Dress.class);
+		Dress dress2 = applicationContext.getBean(Dress.class);
 
-
+		System.out.println("Dress 1: " + dress1);
+		System.out.println("Dress 2: " + dress2);
 	}
 
 }

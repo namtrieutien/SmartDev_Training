79614bf26eedd96e4786a59e745cf47b7172272d
Nguyen Huu Nam <nam.d02th@gmail.com>
2021-06-04 07:33:42 +0700

    - using annotation @Component

--- Java/hello-spring-boot/src/main/java/com/example/demo/HelloSpringBootApplication.java
+++ Java/hello-spring-boot/src/main/java/com/example/demo/HelloSpringBootApplication.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HelloSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringBootApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(HelloSpringBootApplication.class, args);
+		Dress dress = applicationContext.getBean(Dress.class);
+		System.out.println("Instance: " + dress);
+		dress.wear();
 	}
 
 }

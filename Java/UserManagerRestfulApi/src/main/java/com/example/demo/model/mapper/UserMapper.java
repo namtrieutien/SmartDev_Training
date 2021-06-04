f22c8dc58f0dda1e65201ec2e7be490448b75035
Nguyen Huu Nam <nam.d02th@gmail.com>
2021-06-04 15:49:00 +0700

    - set email for userDto.

--- Java/UserManagerRestfulApi/src/main/java/com/example/demo/model/mapper/UserMapper.java
+++ Java/UserManagerRestfulApi/src/main/java/com/example/demo/model/mapper/UserMapper.java
@@ -8,6 +8,7 @@ public class UserMapper {
         UserDto userDto = new UserDto();
         userDto.setId(user.getId());
         userDto.setName(user.getName());
+        userDto.setEmail(user.getEmail());
         userDto.setPhone(user.getPhone());
         userDto.setAvatar(user.getAvatar());
         return userDto;

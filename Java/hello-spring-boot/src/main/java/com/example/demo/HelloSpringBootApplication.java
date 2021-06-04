0ec570872aad19884c93861e9fafca650bf28b1a
Nguyen Huu Nam <nam.d02th@gmail.com>
2021-06-04 07:45:35 +0700

    - using annotation @ComponentScan

--- Java/hello-spring-boot/src/main/java/com/example/demo/HelloSpringBootApplication.java
+++ Java/hello-spring-boot/src/main/java/com/example/demo/HelloSpringBootApplication.java
@@ -1,18 +1,22 @@
 package com.example.demo;
 
+import com.example.demo.other_packet.Bikini;
 import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.example.demo.other_packet")
 @SpringBootApplication
 public class HelloSpringBootApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(HelloSpringBootApplication.class, args);
-		Dress dress = applicationContext.getBean(Dress.class);
-		System.out.println("Instance: " + dress);
-		dress.wear();
+
+		Bikini bikini = applicationContext.getBean(Bikini.class);
+		System.out.println("Instance: " + bikini);
+
 	}
 
 }

2576206b3af8ec0513713049fbb755623a64e7fa
Nguyen Huu Nam <nam.d02th@gmail.com>
2021-06-07 16:58:28 +0700

    - using string data jpa: get data from postgresql databse

--- Java/UserManagerRestfulApi/src/main/java/com/example/demo/controller/UserController.java
+++ Java/UserManagerRestfulApi/src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.User;
 import com.example.demo.model.dto.UserDto;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,16 +17,27 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("")
     public ResponseEntity<?> getListUsers(){
         List<UserDto> userDtos = userService.getListUsers();
         return ResponseEntity.ok(userDtos);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getUserById(@PathVariable int id){
-        UserDto userDtos = userService.getUserById(id);
-        return ResponseEntity.ok(userDtos);
+    @GetMapping("/getuserbyid")
+    public ResponseEntity<?> getUserById(@RequestParam("id") int id){
+        System.out.println("getUserById");
+        User user = userRepository.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/getuserbyname")
+    public ResponseEntity<?> getUserByName(@RequestParam("name") String name){
+        System.out.println("getUserByName");
+        User user = userRepository.getUserByName(name);
+        return ResponseEntity.ok(user);
     }
 
     @GetMapping("/search")
--- Java/UserManagerRestfulApi/src/main/java/com/example/demo/entity/User.java
+++ Java/UserManagerRestfulApi/src/main/java/com/example/demo/entity/User.java
@@ -6,16 +6,32 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "phone")
     private String phone;
+
+    @Column(name = "avatar")
     private String avatar;
+
+    @Column(name = "password")
     private String password;
 }
